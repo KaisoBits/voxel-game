bedd460db40f83e1e3a350e07547ae4b09ff04a2
KaisoBits <31374466+KaisoBits@users.noreply.github.com>
2024-03-02 13:06:47 +0100

    Init window and glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,46 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+constexpr int windowWidth = 800;
+constexpr int windowHeight = static_cast<int>(windowWidth * (9.0/16.0));
+
 int main(int argc, char** argv)
 {
-	return 0;
+	if (!glfwInit())
+	{
+		std::cout << "Failed to initialize GLFW\n";
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Voxel", nullptr, nullptr);
+	if (!window)
+	{
+		std::cout << "Failed to initialize a GLFW window\n";
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+	{
+		std::cout << "Failed to initialize OpenGL\n";
+		return -3;
+	}
+
+	glClearColor(0.3f, 0.3f, 0.8f, 1.0f);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwPollEvents();
+
+
+		glfwSwapBuffers(window);
+	}
 }
