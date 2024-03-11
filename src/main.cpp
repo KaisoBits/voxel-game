a4170d9c2d1ebd05352ebd351d6fdf01a19b65e5
KaisoBits <31374466+KaisoBits@users.noreply.github.com>
2024-03-12 00:10:14 +0100

    Delete duplicate glfw hint

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,6 @@ int main(int argc, char** argv)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Voxel", nullptr, nullptr);
 	if (!window)
