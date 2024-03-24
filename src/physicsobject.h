bae4c18e66c0e873d59068a4b8470c932b934728
KaisoBits <31374466+KaisoBits@users.noreply.github.com>
2024-03-24 19:40:13 +0100

    Fix missing member variable prefix

--- src/physicsobject.h
+++ src/physicsobject.h
@@ -8,14 +8,14 @@ public:
 	explicit PhysicsObject(glm::vec3 position, glm::vec3 mins, glm::vec3 maxs) :
 		m_position(position), m_mins(mins), m_maxs(maxs) {}
 
-	float GetDrag() const { return drag; }
+	float GetDrag() const { return m_drag; }
 	glm::vec3 GetPosition() const { return m_position; }
 	glm::vec3 GetVelocity() const { return m_velocity; }
 	glm::vec3 GetMins() const { return m_mins; }
 	glm::vec3 GetMaxs() const { return m_maxs; }
 	bool GetIsGrounded() const { return m_isGrounded; }
 
-	void SetDrag(float value) { drag = value; }
+	void SetDrag(float value) { m_drag = value; }
 	void SetPosition(const glm::vec3& value) { m_position = value; }
 	void SetVelocity(const glm::vec3& value) { m_velocity = value; }
 	void SetMins(const glm::vec3& value) { m_mins = value; }
@@ -23,7 +23,7 @@ public:
 	void SetIsGrounded(bool value) { m_isGrounded = value; }
 
 private:
-	float drag = 1;
+	float m_drag = 1;
 	glm::vec3 m_velocity{ 0 };
 	glm::vec3 m_position;
 	glm::vec3 m_mins;
